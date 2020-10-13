08e0a6ad8ae6740058b92b0d54b6b90dab276118
Hardlygo <961332109@qq.com>
2020-10-13 22:25:03 +0800

    Updates

---
+++ precache-manifest.9dfe7282d18670f8c8586e8754ac605c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ae0c926843bdd6b1ce0a028bd12e18",
+    "url": "/my-react-app-sale/index.html"
+  },
+  {
+    "revision": "0826232a688ee9229d30",
+    "url": "/my-react-app-sale/static/css/3.03571c8d.chunk.css"
+  },
+  {
+    "revision": "aa1f123d365f59f336d3",
+    "url": "/my-react-app-sale/static/css/4.416a6096.chunk.css"
+  },
+  {
+    "revision": "174fd4a25bebc7f48d0c",
+    "url": "/my-react-app-sale/static/css/5.57b66b2d.chunk.css"
+  },
+  {
+    "revision": "2a3fc0483c61be694f59",
+    "url": "/my-react-app-sale/static/css/6.2f797fc2.chunk.css"
+  },
+  {
+    "revision": "99957fa76d71145e33ee",
+    "url": "/my-react-app-sale/static/css/main.9ae43e9b.chunk.css"
+  },
+  {
+    "revision": "88008d3df5b20455b91f",
+    "url": "/my-react-app-sale/static/js/2.298a7820.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app-sale/static/js/2.298a7820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0826232a688ee9229d30",
+    "url": "/my-react-app-sale/static/js/3.d364034b.chunk.js"
+  },
+  {
+    "revision": "aa1f123d365f59f336d3",
+    "url": "/my-react-app-sale/static/js/4.5a88d144.chunk.js"
+  },
+  {
+    "revision": "174fd4a25bebc7f48d0c",
+    "url": "/my-react-app-sale/static/js/5.e3ebb765.chunk.js"
+  },
+  {
+    "revision": "2a3fc0483c61be694f59",
+    "url": "/my-react-app-sale/static/js/6.ebceab90.chunk.js"
+  },
+  {
+    "revision": "99957fa76d71145e33ee",
+    "url": "/my-react-app-sale/static/js/main.c1172cdb.chunk.js"
+  },
+  {
+    "revision": "92ca8b68292cc2d830f7",
+    "url": "/my-react-app-sale/static/js/runtime-main.2ae441c0.js"
+  },
+  {
+    "revision": "0f98adacb38829a9c80f957fcdaf00ab",
+    "url": "/my-react-app-sale/static/media/iconfont.0f98adac.eot"
+  },
+  {
+    "revision": "5c25e1cb9735adf1b556f42b8df9b7d1",
+    "url": "/my-react-app-sale/static/media/iconfont.5c25e1cb.svg"
+  },
+  {
+    "revision": "8b4280522f40061b4dbd8324435281f9",
+    "url": "/my-react-app-sale/static/media/iconfont.8b428052.ttf"
+  }
+]);
